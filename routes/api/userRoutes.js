a183fd972395a97b20bb24946f6f754b9c97d104
Jen Truong <jennifertruong09@gmail.com>
2022-03-17 16:16:09 -0400

    added some additional comments to the routes

--- routes/api/userRoutes.js
+++ routes/api/userRoutes.js
@@ -10,14 +10,17 @@ const {
     removeFriend
 } = require('../../controllers/userController');
 
+// /api/users/
 router.route('/').get(getUsers).post(createUser);
 
+// /api/users/:userId
 router
     .route('/:userId')
     .get(getSingleUser)
     .put(updateUser)
     .delete(deleteUser);
 
+// /api/users/:userId/friends/:friendId
 router.route('/:userId/friends/:friendId').post(addFriend).delete(removeFriend);
 
 module.exports = router;
